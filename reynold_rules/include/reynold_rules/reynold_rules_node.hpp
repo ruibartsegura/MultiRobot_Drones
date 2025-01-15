542c49c0e13a8c3b9ffb610b25d76d25cea0c32d
aira534 <ay.elio.2022@alumnos.urjc.es>
2025-01-16 00:19:40 +0100

    array odom

--- reynold_rules/include/reynold_rules/reynold_rules_node.hpp
+++ reynold_rules/include/reynold_rules/reynold_rules_node.hpp
@@ -2,25 +2,83 @@
 #define REYNOLD_RULES__REYNOLDRULESNODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+
+#include <vector>
+#include <queue>
+#include <set>
 
 namespace reynold_rules
 {
 
 class ReynoldRulesNode : public rclcpp::Node
 {
-public:  
+public:
+  
   RCLCPP_SMART_PTR_DEFINITIONS(ReynoldRulesNode)
   
   ReynoldRulesNode();
   void control_cycle();
 
-  std::vector<float> separation_rule();
-  std::vector<float> aligment_rule();
-  std::vector<float> cohesion_rule();
-  std::vector<float> nav_2_point_rule();
-  std::vector<float> avoidance_rule();
+  std::vector<geometry_msgs::msg::Vector3> separation_rule();
+  std::vector<geometry_msgs::msg::Vector3> aligment_rule();
+  std::vector<geometry_msgs::msg::Vector3> cohesion_rule();
+  std::vector<geometry_msgs::msg::Vector3> nav_2_point_rule();
+  std::vector<geometry_msgs::msg::Vector3> avoidance_rule();
 
 private:
+  static const int NUMBER_DRONES {4};
+  double MAX_LIN_VEL {0.3};
+  double DIST_THRESHOLD {0.3};
+  std::vector<nav_msgs::msg::Odometry::SharedPtr> robots_;
+
+  // Map
+  nav_msgs::msg::OccupancyGrid::SharedPtr map_;
+
+  // Separation
+  int view_range_;
+  double get_distance(geometry_msgs::msg::Point pos1, geometry_msgs::msg::Point pos2);
+  geometry_msgs::msg::Vector3 calc_sep_vector(geometry_msgs::msg::Point position, int num);
+
+  // Alignment
+  geometry_msgs::msg::Vector3 calc_average_velocity();
+
+  // Cohesion
+  geometry_msgs::msg::Point calc_average_pos(std::vector<nav_msgs::msg::Odometry>);
+  geometry_msgs::msg::Vector3 calc_cohesion_vector(geometry_msgs::msg::Point robot_pos);
+
+  // Nav_2_Point
+  std::vector<geometry_msgs::msg::Point> findPathThroughWaypoints(
+      const geometry_msgs::msg::Point& start, const geometry_msgs::msg::Point& target);
+
+  std::vector<geometry_msgs::msg::Point> findNeighbors(
+    const std::vector<geometry_msgs::msg::Point>& waypoints, const geometry_msgs::msg::Point& currentWp, int step = 2);
+
+  bool isPathClear(const std::pair<int, int>& start, const std::pair<int, int>& end);
+
+  geometry_msgs::msg::Vector3 vector_2_points(geometry_msgs::msg::Point point1, geometry_msgs::msg::Point point2);
+
+  geometry_msgs::msg::Point target_point;
+  geometry_msgs::msg::Point prev_point;
+  std::vector<geometry_msgs::msg::Point> waypoints_;
+  std::vector<geometry_msgs::msg::Point> path_;
+
+  // Subscribers
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr data);
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr drones_sub1_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr drones_sub2_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr drones_sub3_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr drones_sub4_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr drones_sub_;
+
+  // void map_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr data);
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
+  
+
+  void checkPathsBetweenWaypoints();
 
   rclcpp::TimerBase::SharedPtr timer_;
 };
--- reynold_rules/src/reynold_rules/reynold_rules_node.cpp
+++ reynold_rules/src/reynold_rules/reynold_rules_node.cpp
@@ -13,8 +13,54 @@ namespace reynold_rules
 ReynoldRulesNode::ReynoldRulesNode()
 : Node("publisher_node")
 {
+  // Make sure the array of the odom for the drones is of the correct size
+  if (robots_.size() <= NUMBER_DRONES) {
+      robots_.resize(NUMBER_DRONES);  // Redimensiona si es necesario
+  }
+
+  for (int n = 1; n <= NUMBER_DRONES; n++) {
+    std::string topic_name = "/cf_" + std::to_string(n) + "/odom";
+  }
+  drones_sub1_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/cf_1/odom", 
+      10,
+      std::bind(&ReynoldRulesNode::odom_callback, this, std::placeholders::_1)
+  );
+  drones_sub2_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/cf_2/odom", 
+      10,
+      std::bind(&ReynoldRulesNode::odom_callback, this, std::placeholders::_1)
+  );
+  drones_sub3_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/cf_3/odom", 
+      10,
+      std::bind(&ReynoldRulesNode::odom_callback, this, std::placeholders::_1)
+  );
+  drones_sub4_ = create_subscription<nav_msgs::msg::Odometry>(
+      "/cf_4/odom", 
+      10,
+      std::bind(&ReynoldRulesNode::odom_callback, this, std::placeholders::_1)
+  );
+
+  // map_sub_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
+  //    "/map", 
+  //    10,
+  //    std::bind(&ReynoldRulesNode::map_callback, this, std::placeholders::_1)
+  //);
+
   timer_ = create_wall_timer(
     500ms, std::bind(&ReynoldRulesNode::control_cycle, this));
+}
+
+void
+ReynoldRulesNode::odom_callback(const nav_msgs::msg::Odometry::SharedPtr data)
+{
+    std::string number = data->child_frame_id.substr(std::strlen("cf_"));
+
+    // Asignación de SharedPtr en el vector
+    int drone_number = std::stoi(number);
+
+    robots_[drone_number] = data;
 }
 
 void
@@ -25,7 +71,7 @@ ReynoldRulesNode::control_cycle()
   }
 }
 
-std::vector<float>
+std::vector<geometry_msgs::msg::Vector3>
 ReynoldRulesNode::separation_rule()
 {
   while (true){
@@ -33,7 +79,7 @@ ReynoldRulesNode::separation_rule()
   }
 }
 
-std::vector<float>
+std::vector<geometry_msgs::msg::Vector3>
 ReynoldRulesNode::aligment_rule()
 {
   while (true){
@@ -41,7 +87,7 @@ ReynoldRulesNode::aligment_rule()
   }
 }
 
-std::vector<float>
+std::vector<geometry_msgs::msg::Vector3>
 ReynoldRulesNode::cohesion_rule()
 {
   while (true){
@@ -49,7 +95,7 @@ ReynoldRulesNode::cohesion_rule()
   }
 }
 
-std::vector<float>
+std::vector<geometry_msgs::msg::Vector3>
 ReynoldRulesNode::nav_2_point_rule()
 {
   while (true){
@@ -57,7 +103,7 @@ ReynoldRulesNode::nav_2_point_rule()
   }
 }
 
-std::vector<float>
+std::vector<geometry_msgs::msg::Vector3>
 ReynoldRulesNode::avoidance_rule()
 {
   while (true){

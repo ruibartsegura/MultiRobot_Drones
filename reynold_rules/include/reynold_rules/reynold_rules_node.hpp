0ec0d78ad5a73b6fca3c2f1f5c4d41313c739f27
aira534 <ay.elio.2022@alumnos.urjc.es>
2025-01-17 21:22:05 +0100

    obstacle avoidance except control cycle

--- reynold_rules/include/reynold_rules/reynold_rules_node.hpp
+++ reynold_rules/include/reynold_rules/reynold_rules_node.hpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <array>
 
 namespace reynold_rules
 {
@@ -33,6 +34,7 @@ public:
 
 private:
   static const int NUMBER_DRONES {4};
+  static constexpr double ANGULAR_STEPSIZE {2 * (M_PI / 180.0)};
   double MAX_LIN_VEL {0.3};
   double DIST_THRESHOLD {0.3};
   std::vector<nav_msgs::msg::Odometry::SharedPtr> robots_;
@@ -41,7 +43,7 @@ private:
   nav_msgs::msg::OccupancyGrid::SharedPtr map_;
 
   // Separation
-  int view_range_;
+  double view_range_;
   double get_distance(geometry_msgs::msg::Point pos1, geometry_msgs::msg::Point pos2);
   geometry_msgs::msg::Vector3 calc_sep_vector(geometry_msgs::msg::Point position, int num);
 
@@ -49,8 +51,15 @@ private:
   geometry_msgs::msg::Vector3 calc_average_velocity();
 
   // Cohesion
-  geometry_msgs::msg::Point calc_average_pos(std::vector<nav_msgs::msg::Odometry>);
+  geometry_msgs::msg::Point calc_average_pos(std::vector<nav_msgs::msg::Odometry> positions);
   geometry_msgs::msg::Vector3 calc_cohesion_vector(geometry_msgs::msg::Point robot_pos);
+
+  // Obstacle avoidance
+  double view_angle_;
+  double view_split_;
+  bool map_lookup(geometry_msgs::msg::Point& pos);
+  std::vector<geometry_msgs::msg::Point> find_obstacles(const geometry_msgs::msg::Pose robot_pose);
+  geometry_msgs::msg::Vector3 sum_weighted_repellent_vectors(int robot_index);
 
   // Nav_2_Point
   std::vector<geometry_msgs::msg::Point> findPathThroughWaypoints(

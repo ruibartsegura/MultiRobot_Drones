40495c1a257c182da4c4fc3b461bab4c0054aea4
jagutic <ja.gutierrezc.2022@alumnos.urjc.es>
2025-02-06 11:10:22 +0100

    Change demo formation

--- ctrl/demo_formation.sh
+++ ctrl/demo_formation.sh
@@ -13,16 +13,18 @@ ros2 launch $LAUNCH & #> $LOG 2>&1 &
 proc_id=$!
 echo "Spawned pid $! - logging to $LOG"
 
-echo "Press enter to continue: navigate to (0, 1.5)"; read
-ros2 param set /reynold_rules_node target_point      [0.0,-1.0,1.0]
-ros2 param set /reynold_rules_node nav2point_weight  0.2
+end="n";
+while [ $end != "y" ]; do
+    echo "Enter x and y coord. Press enter to continue"; read x y
+    ros2 param set /reynold_rules_node nav2point_weight  0.2
+    ros2 param set /reynold_rules_node target_point      [$x,$y,1.0]
 
-echo "Press enter to continue: formation line"; read
-ros2 param set /reynold_rules_node nav2point_weight  0.0
-ros2 param set /reynold_rules_node formation_type    2
+    echo "Press enter to continue: formation line"; read type
+    ros2 param set /reynold_rules_node nav2point_weight  0.0
+    ros2 param set /reynold_rules_node formation_type    $type
 
-echo "Press enter to continue: formation triangle"; read
-ros2 param set /reynold_rules_node formation_type    3
+    echo "Do you want to finish? (y/n): "; read end
+done
 
 echo "Press enter to continue: stop reynold_rules_node"; read
 killall reynold_rules_node --signal SIGINT
